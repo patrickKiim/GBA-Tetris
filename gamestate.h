e2bff15fb088031a864efec39e6a478e0d76c405
leonie0206 <129604103+leonie0206@users.noreply.github.com>
2023-04-22 13:04:16 +0800

    Update gamestate.h

--- gamestate.h
+++ gamestate.h
@@ -1,34 +1,36 @@
-#include "drawsprites.h"
+
+#include "mygbalib.h"
+
 
 
 
 int score = 0; //current score
 int game_over = 0; //Make iot so this varibale is linked to a function stating whether the game is over
-int gamestate = 0;
+
 
 void main_screen() {
     int first = 80;
-    int second = 96;
+    int second = 120;
 
     // Draw “TETRIS” “PRESS A TO START”
     drawSprite(10, 0, 72, first); //T
-    drawSprite(11, 1, 88, first);
-    drawSprite(12, 2, 104, first);
-    drawSprite(13, 3, 120, first);
-    drawSprite(14, 4, 136, first);
-    drawSprite(15, 5, 152, first);
+    drawSprite(11, 1, 92, first); //E
+    drawSprite(12, 2, 112, first); //T
+    drawSprite(13, 3, 132, first); //R
+    drawSprite(14, 4, 152, first);
+    drawSprite(15, 5, 172, first);
     drawSprite(16, 6, 32, second);
-    drawSprite(17, 7, 48, second);
-    drawSprite(18, 8, 64, second);
-    drawSprite(19, 9, 80, second);
-    drawSprite(19, 10, 112, second);
-    drawSprite(19, 11, 128, second);
-    drawSprite(20, 12, 144, second);
-    drawSprite(21, 13, 160, second);
-    drawSprite(22, 14, 176, second);
-    drawSprite(20, 15, 192, second);
+    drawSprite(17, 7, 50, second);
+    drawSprite(18, 8, 68, second);
+    drawSprite(19, 9, 86, second);
+    drawSprite(19, 10, 122, second);
+    drawSprite(19, 11, 140, second);
+    drawSprite(20, 12, 158, second);
+    drawSprite(21, 13, 176, second);
+    drawSprite(13, 14, 194, second);
+    drawSprite(20, 15, 212, second); 
     //draw “TETRIS” “PRESS A TO START”
-    //How do I know if this button is pressed? --> check button function --> change g ==1?
+  
 } 
 
 void level_1(){
@@ -38,7 +40,7 @@ void level_1(){
     int k = 10; // Sprite number right
 
     // Draw lines on the right and left using for loop
-    for(i = 0; i < 160; i += 16) {
+    for(i; i < 160; i += 16) {
         drawSprite(22, j, 0, i);
         drawSprite(22, k, 96, i);
         j++;
@@ -75,6 +77,9 @@ void level_2(){
         j++;
         k++;
     }
+
+// Draw Level2
+    drawSprite(2, 20, 208, 16);
 
     while(!game_over) {
         // Backend Game logic!
