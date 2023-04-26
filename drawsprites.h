78b55b84b6f6ec76a435ea679e9436470d4d25cf
patrickKiim <s524kim@uwaterloo.ca>
2023-04-26 15:00:19 +0800

    deleting non-neeeded files before merge

--- drawsprites.h
+++
@@ -1,7 +0,0 @@
-void drawSprite(int numb, int N, int x, int y)
-{
-    // Gift function: displays sprite number numb on screen at position (x,y), as sprite object N
-    *(unsigned short *)(0x7000000 + 8*N) = y | 0x2000;
-    *(unsigned short *)(0x7000002 + 8*N) = x;
-    *(unsigned short *)(0x7000004 + 8*N) = numb*2;
-}
